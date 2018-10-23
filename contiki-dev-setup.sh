ca32f63c17a6d699647039570c9eff94f4f95347
khongyew <khongyew@gmail.com>
2018-10-23 22:12:38 +0800

    some updates to contiki setup script

--- contiki-dev-setup.sh
+++ contiki-dev-setup.sh
@@ -2,6 +2,15 @@ echo -e "\e[93m Installing toolchain for contiki development \e[0m"
 
 # Install ARM toolchain
 wget https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2
+
 tar xjf gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2 -C /tmp/
-sudo cp -f -r /tmp/gcc-arm-none-eabi-5_2-2015q4/* /usr/local/
+
+sudo mkdir /opt/gcc-arm-none-eabi-5_2-2015q4/
+
+sudo cp -f -r /tmp/gcc-arm-none-eabi-5_2-2015q4/* /opt/gcc-arm-none-eabi-5_2-2015q4/
+
 rm -rf /tmp/gcc-arm-none-eabi-* gcc-arm-none-eabi-*-linux.tar.bz2
+
+echo 'export PATH="$PATH:/opt/gcc-arm-none-eabi-5_2-2015q4/bin"' >> ~/.profile
+
+echo $PATH
