caaad2c1b37914c936ac17cc71066fa03b8e578e
khongyew <khongyew@gmail.com>
2019-03-23 22:07:49 +0800

    add python pip entry

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -9,7 +9,9 @@ apt-get -y install git
 apt-get -y install vim
 apt-get -y install default-jdk
 apt-get -y install python2.7
+apt-get -y install python-pip
 apt-get -y install python3
+apt-get -y install python3-pip
 apt-get -y install net-tools #ifconfig is no longer packaged with ubuntu 18.04
 apt-get -y install nodejs
 apt-get -y install npm
